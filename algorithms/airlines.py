87596209e496b387ea5f4e90fc374301e44d35d2
Kavey-Li <s4628614@student.uq.edu.au>
2023-10-06 17:53:49 +1000

    Create

--- algorithms/airlines.py
+++ algorithms/airlines.py
@@ -106,39 +106,45 @@ def enumerate_hubs(graph: Graph, min_degree: int) -> ExtensibleList:
 
 def calculate_flight_budget(graph: Graph, origin: int, stopover_budget: int, monetary_budget: int) -> ExtensibleList:
     # Initialize distances and stopovers with infinity and -1 respectively
-    distances = [float('inf')] * len(graph._nodes)
-    stopovers = [-1] * len(graph._nodes)
-    distances[origin] = 0
-    stopovers[origin] = 0
+    distances = ExtensibleList()
+    stopovers = ExtensibleList()
+    for _ in graph._nodes:
+        distances.append(float('inf'))
+        stopovers.append(-1)
+    distances.set_at(origin, 0)
+    stopovers.set_at(origin, 0)
 
     # Priority queue: (cost, stopovers, node_id)
     queue = PriorityQueue()
-    queue.insert(0, (0, origin))
+    queue.insert(0, origin)
 
     while not queue.is_empty():
         current_cost, current_node = queue.remove_min()
-        current_stopover = stopovers[current_node]
+        current_stopover = stopovers.get_at(current_node)
 
         # If we've already processed a better path to this node, skip
-        if current_cost > distances[current_node]:
+        if current_cost > distances.get_at(current_node):
             continue
 
-        for neighbour, edge_cost in graph.get_neighbours(current_node):  # Unpack the tuple here
+        for neighbour, edge_cost in graph.get_neighbours(current_node):
             neighbour_id = neighbour.get_id()
 
             # If we can reach the neighbour with fewer stopovers and cost, update
-            if current_stopover + 1 <= stopover_budget and current_cost + edge_cost < distances[neighbour_id]:
-                distances[neighbour_id] = current_cost + edge_cost
-                stopovers[neighbour_id] = current_stopover + 1
-                queue.insert(distances[neighbour_id], (distances[neighbour_id], neighbour_id))
+            if current_stopover + 1 <= stopover_budget and current_cost + edge_cost < distances.get_at(neighbour_id):
+                distances.set_at(neighbour_id, current_cost + edge_cost)
+                stopovers.set_at(neighbour_id, current_stopover + 1)
+                queue.insert(distances.get_at(neighbour_id), neighbour_id)
 
     # Filter destinations based on monetary budget and sort them
     destinations = ExtensibleList()
-    for i, cost in enumerate(distances):
-        if cost <= monetary_budget:
-            destinations.append((i, cost, stopovers[i]))
+    i = 0
+    while i < distances.get_size():
+        if distances.get_at(i) <= monetary_budget:
+            destinations.append((i, distances.get_at(i), stopovers.get_at(i)))
+        i += 1
 
-    destinations.sort(key=lambda x: (x[1], x[2], x[0]))
+    # Sort destinations using the provided sort method in ExtensibleList
+    destinations.sort()
 
     return destinations
 
